3048f9833a0234108197829372901e9cb2619828
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-15 16:08:18 +0300

    change database

--- patashida/settings.py
+++ patashida/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'patashida.wsgi.application'
 DATABASES = {
      'default': {
        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'agric',
+        'NAME':'agriculture',
         'USER':'shalin',
         'PASSWORD':'Chepkoech03',
     }
