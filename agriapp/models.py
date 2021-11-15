67868702764b78f699546ba7fdb5cb1f8fc6ba35
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-15 16:47:04 +0300

    remove category class

--- agriapp/models.py
+++ agriapp/models.py
@@ -10,11 +10,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return f"{ self.name }"
 
 
 
