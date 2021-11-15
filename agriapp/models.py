8ec173f84b2138ebd66b33d4d2ce02bec12082bc
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-15 12:25:47 +0300

    create django user model in models file

--- agriapp/models.py
+++ agriapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import datetime as dt
 from django.urls import reverse
+from django.contrib.auth.models import User
 from url_or_relative_url_field.fields import URLOrRelativeURLField
 
 # cloudinary
