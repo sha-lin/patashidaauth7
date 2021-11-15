82e3165e43dda508ed49988ea708156e27942504
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-15 11:26:23 +0300

    enable corsheaders loaclhost url

--- patashida/settings.py
+++ patashida/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'the deployed url',
+    "http://localhost:8000",
 ]
 
 ROOT_URLCONF = 'patashida.urls'
