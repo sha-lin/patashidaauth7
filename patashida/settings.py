c23bcbb03ff0d11b3133ed294c8fbc47d8be9992
sha-lin <shalin.rono@student.moringaschool.com>
2021-11-15 16:16:56 +0300

    change localhost

--- patashida/settings.py
+++ patashida/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
-    'https://localhost:4200/',
+    'https://localhost:8000/',
 ]
 
 ROOT_URLCONF = 'patashida.urls'
